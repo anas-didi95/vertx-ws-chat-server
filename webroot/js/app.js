b24e666439fe782908829e5c1a2e87bb8d6b7f3a
Anas Juwaidi <anas.didi95@gmail.com>
2021-02-09 20:47:38 +0800

    Configure websocket to send message and refresh chat

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,3 +1,30 @@
+let eventBus;
+let username;
+
+function init() {
+  registerHandler();
+}
+
+function registerHandler() {
+  console.log("registerHandler");
+  eventBus = new EventBus("http://localhost:5000/eventbus");
+  eventBus.onopen = function () {
+    eventBus.registerHandler('out', function (error, message) {
+      const counter = message.body;
+      document.getElementById('current_value').innerHTML = counter;
+    });
+    eventBus.registerHandler("ws-refresh-chat", function (error, message) {
+      console.log("[registerHandler][ws-refreh-chat] message", message);
+      console.log("[registerHandler][ws-refreh-chat] error", error);
+    });
+  }
+}
+
+function increment() {
+  console.log("increament");
+  eventBus.send("in", { "body": "hello" }, "headers");
+}
+
 const App = {
   data() {
     return {
@@ -11,30 +38,12 @@ const App = {
   methods: {
     sendMessage() {
       console.log("message", this.message)
+      eventBus.send("ws-send-message", {
+        "username": this.username,
+        "message": this.message
+      })
     }
   }
 }
-
 Vue.createApp(App).mount("#app")
 
-let eventBus;
-let username;
-
-function init() {
-  registerHandler();
-}
-
-function registerHandler() {
-  eventBus = new EventBus("http://localhost:5000/eventbus");
-  eventBus.onopen = function () {
-    eventBus.registerHandler('out', function (error, message) {
-      const counter = message.body;
-      document.getElementById('current_value').innerHTML = counter;
-    });
-  }
-}
-
-function increment() {
-  console.log("increament");
-  eventBus.send("in", { "body": "hello" }, "headers");
-}
